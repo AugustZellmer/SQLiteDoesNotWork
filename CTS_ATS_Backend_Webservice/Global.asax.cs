0ad0950534e786fc4a8eaf0ceea23320d70ed950
August Zellmer <azellmer@consult-cts.com>
2020-05-26 18:34:38 -0500

    init commit

---
+++ CTS_ATS_Backend_Webservice/Global.asax.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace Broken
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            var db = new SqliteConnection("Data Source=testDB.sqlite");
+            db.Open();
+        }
+    }
+}
